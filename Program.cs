b0c65edee8b14140c3dd093ee9d4db676119370a
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-01-04 20:20:18 -0300

    finalizando desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123999", modelo: "Modelo 1", imei: "123456789", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "1288", modelo: "Modelo 2", imei: "987654321", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DioApp");
